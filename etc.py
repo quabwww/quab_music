3d62b8399d24d7d866823b81b7f86f96667c5079
quabwww <josuebuenovargas@gmail.com>
2024-07-19 20:10:36 +0200

    d

--- etc.py
+++ etc.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://localhost:9000/api/musica/"
+url = "https://musicscript.onrender.com/api/musica/"
 data = {
     "guild_id": 1077968892535775262,
     "channel_id": 1100148368996573265,
